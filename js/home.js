4546eecb5412947af74ed08dfa9b4045ef76410c
md-ahmodullah <contactme.ahmod@gmail.com>
2024-09-19 09:38:58 +0600

    Added Add Money functionality.

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log('home got it');
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const pin = document.getElementById('pin').value;
+    if (pin == '1234') {
+        const addAmount = Number(document.getElementById('add-amount').value);
+        const currentBalance = Number(document.getElementById('current-balance').innerText);
+        const newBalance = addAmount + currentBalance;
+
+        document.getElementById('current-balance').innerText = newBalance;
+        
+    }
+    else{
+        alert("Please Input Correct PIN.");
+    }
+})
